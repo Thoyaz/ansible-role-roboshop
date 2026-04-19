8dadf48785104e09ec9551cdf8aba52bc2ee7acd
Thoyaz <thoyazthoyazpc@gmail.com>
2026-04-19 18:48:53 +0530

    Ansible Roboshop

---
+++ Notes.sh
@@ -0,0 +1,6 @@
+Ansible directory structure:
+===============================
+https://docs.ansible.com/projects/ansible/latest/playbook_guide/playbooks_reuse_roles.html
+https://docs.ansible.com/projects/ansible/latest/playbook_guide/playbooks_reuse_roles.html
+
+
